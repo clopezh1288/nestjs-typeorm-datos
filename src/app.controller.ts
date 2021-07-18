e9fd01546262dd1a367e843e8d90051a89bced85
Carlos Lopez <c.lopezh1288@gmail.com>
2021-07-18 14:29:23 -0600

    paso 3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -18,5 +18,10 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'con /sas/';
+  }
+
+  @Get('tasks')
+  tasks() {
+    return this.appService.getTasks();
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,10 @@
 import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+import config from 'src/config';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
-
 @Global()
 @Module({
   providers: [
@@ -10,7 +12,25 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port } = configService.postgres;
+        console.info('port: ', port);
+        const client = new Client({
+          user: user,
+          host: host,
+          database: dbName,
+          password: password,
+          port: port,
+        });
+
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
